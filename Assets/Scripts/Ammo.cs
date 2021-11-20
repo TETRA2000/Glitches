fb497746f33b0c19232b4aaf44457a8a80ad12ca
TETRA2000 <sagittariusm25@gmail.com>
2021-11-20 21:19:12 +0900

    Fire an ammo

---
+++ Assets/Scripts/Ammo.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.MovePosition(transform.position + -1 * transform.up * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject fireball;
     public float moveSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
@@ -19,5 +19,11 @@ public class Player : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime);
+
+        if(Input.GetKeyDown("space"))
+        {
+            var fire = Instantiate(fireball);
+            fire.transform.position = transform.position;
+        }
     }
 }
