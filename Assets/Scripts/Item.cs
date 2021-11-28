50897d2b94da8fd75000e59fed7662dedfd0e794
TETRA2000 <sagittariusm25@gmail.com>
2021-11-28 20:12:14 +0900

    Minor modification

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -8,6 +8,16 @@ public class Item : MonoBehaviour
     private int playerLayer, ammoLayer;
     private UnityEvent glitchEvent;
 
+    public enum GlitchMode { Normal, DisableGuns }
+    public GlitchMode glitchMode;
+
+    private float spawnTime;
+
+    private SpriteRenderer spriteRenderer;
+
+    private bool acitivated = false;
+    private float activationWaitTime = 0.3f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,21 +26,46 @@ public class Item : MonoBehaviour
 
         playerLayer = LayerMask.NameToLayer("Player");
         ammoLayer = LayerMask.NameToLayer("Ammo");
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        spawnTime = Time.time;
+        spriteRenderer.color = new Color(1f, 1f, 1f, 0.2f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!acitivated && Time.time - spawnTime > activationWaitTime)
+        {
+            spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+            acitivated = true;
+        }    
         
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (Time.time - spawnTime < activationWaitTime) return;
+
+
         if(collision.gameObject.layer == playerLayer ||
             collision.gameObject.layer == ammoLayer)
         {
             Destroy(gameObject);
-            glitchEvent.Invoke();
+            ActivateGlitch();
+        }
+    }
+
+    private void ActivateGlitch()
+    {
+        switch(glitchMode)
+        {
+            case GlitchMode.Normal:
+                glitchEvent.Invoke();
+                break;
+            case GlitchMode.DisableGuns:
+                break;
         }
     }
 }
